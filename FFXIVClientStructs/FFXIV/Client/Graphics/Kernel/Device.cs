0704161513847b32e5983f8bc0cd5dfdc7848495
Asriel Camora <asriel@camora.dev>
2024-05-15 19:42:59 -0700

    Add extra kernel device fields

--- FFXIVClientStructs/FFXIV/Client/Graphics/Kernel/Device.cs
+++ FFXIVClientStructs/FFXIV/Client/Graphics/Kernel/Device.cs
@@ -8,6 +8,14 @@ namespace FFXIVClientStructs.FFXIV.Client.Graphics.Kernel;
 public unsafe partial struct Device {
     [FieldOffset(0x8)] public void* ContextArray; // Client::Graphics::Kernel::Context array
     [FieldOffset(0x10)] public void* RenderThread; // Client::Graphics::Kernel::RenderThread
+    [FieldOffset(0x28)] public ShaderResourceList2* List2_1;
+    [FieldOffset(0x30)] public ShaderResourceList2* List2_2;
+    [FieldOffset(0x38)] public ShaderResourceList2* List2_3;
+    [FieldOffset(0x40)] public ShaderResourceList2* List2_4;
+    [FieldOffset(0x48)] public ShaderResourceList2* List2_5;
+    [FieldOffset(0x50)] public ShaderResourceList* RegisteredConstantBuffers;
+    [FieldOffset(0x58)] public ShaderResourceList* RegisteredSamplers;
+    [FieldOffset(0x69)] public ShaderResourceList* RegisteredUnk;
     [FieldOffset(0x70)] public SwapChain* SwapChain;
     [FieldOffset(0x7A)] public byte RequestResolutionChange;
     [FieldOffset(0x88)] public uint Width;
@@ -21,6 +29,8 @@ public unsafe partial struct Device {
     [FieldOffset(0x1C0)] public uint NewWidth;
     [FieldOffset(0x1C4)] public uint NewHeight;
     [FieldOffset(0x1C8)] public int FrameRate;
+    [FieldOffset(0x1E8)] public ShaderResourceList2* List2_6;
+    [FieldOffset(0x1F0)] public ShaderResourceList2* List2_7;
     [FieldOffset(0x220)] public int D3DFeatureLevel; // D3D_FEATURE_LEVEL enum
     [FieldOffset(0x228)] public void* DXGIFactory; // IDXGIFactory1
     [FieldOffset(0x230)] public void* DXGIOutput; // IDXGIOutput6
@@ -36,4 +46,25 @@ public unsafe partial struct Device {
 
     [MemberFunction("E8 ?? ?? ?? ?? 8B 0F 48 8D 54 24")]
     public partial Texture* CreateTexture2D(int* size, byte mipLevel, uint textureFormat, uint flags, uint unk);
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x50)]
+public unsafe partial struct ShaderResourceList {
+    [FieldOffset(0x00)] public void* VTable;
+    [FieldOffset(0x08)] public fixed byte UnorderedContainer[0x10]; // Some unordered_... container
+    [FieldOffset(0x18)] public fixed byte CriticalSection[0x28];
+    [FieldOffset(0x40)] public StdString* Names; // 0x20 * ReserveSize
+    [FieldOffset(0x48)] public uint Count;
+    [FieldOffset(0x4C)] public uint ReserveSize;
+
+    public readonly Span<StdString> NamesSpan => new(Names, (int)Count);
+}
+
+[StructLayout(LayoutKind.Explicit, Size = 0x40)]
+public unsafe partial struct ShaderResourceList2 {
+    [FieldOffset(0x00)] public void* VTable;
+    [FieldOffset(0x08)] public fixed byte CriticalSection[0x28];
+    [FieldOffset(0x30)] public void* Ptr; // 0x10 * ReserveSize
+    [FieldOffset(0x38)] public uint ReserveSize;
+    [FieldOffset(0x3C)] public uint Unk3C;
 }
